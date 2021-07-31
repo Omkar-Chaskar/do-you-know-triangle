980c926b1df714a33b88302ec75ce2c07893cd29
Omkar-Chaskar <Omkarchaskar1402@gmail.com>
2021-07-31 18:12:08 +0530

    calculate Area of Triangle Done

---
+++ calcarea.js
@@ -0,0 +1,27 @@
+var inputFArea = document.querySelector("#fangle");
+var inputSArea = document.querySelector("#sangle");
+var inputTArea = document.querySelector("#tangle")
+var btnCalcArea = document.querySelector("#btn-calc-area");
+var errMsg = document.querySelectorAll("err-msg");
+
+btnCalcArea.addEventListener("click", function (){
+    var a = Number(inputFArea.value);
+    var b = Number(inputSArea.value);
+    var c = Number(inputTArea.value);
+
+    if ((a+b)>c && (a+c)>b && (b+c)>a ) {
+        var s = (a+b+c)/2;
+
+        a = s-a;
+        b = s-b;
+        c = s-c;
+        console.log(s);
+        var area =  Math.sqrt(s*(a)*(b)*(c))
+        console.log(area)
+        document.querySelector("#msg").innerHTML = area;
+    } else {
+        document.querySelector("#msg").innerHTML = "please enter values in order of Addition of two values are greater than third value";
+    }
+
+    
+})
