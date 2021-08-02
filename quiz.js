249839366ce7bcc1c3339fc0ff83956087fa8a6b
Omkar-Chaskar <Omkarchaskar1402@gmail.com>
2021-08-02 19:21:15 +0530

    quiz js complete

--- quiz.js
+++ quiz.js
@@ -1,13 +1,34 @@
+
 const btnSubmit = document.querySelector("#btn-submit");
-const quizCheck = document.querySelectorAll("quiz")
 var errMsg =document.querySelector("#err-msg")
+var finalScore = document.querySelector("#score")
+let score = 0;
 
+var q1= document.quizes.q1;
+var q2= document.quizes.q2;
+var q3= document.quizes.q3;
+var q4= document.quizes.q4;
+var q5= document.quizes.q5;
+var q6= document.quizes.q6;
+var q7= document.quizes.q7;
+var q8= document.quizes.q8;
+var q9= document.quizes.q9;
+var q10= document.quizes.q10;
 
-btnSubmit.addEventListener("click", function (){
-    if (quizCheck.checked) {
-        errMsg.innerHTML = "Please select all answer."
-        console.log("hiii")
-    } else {
-        console.log("gooo")
+var quesArray = [q1 , q2, q3, q4 ,q5 ,q6 ,q7 ,q8 ,q9 ,q10];
+btnSubmit.addEventListener("click", function(e) {
+    e.preventDefault()
+    for(let i=0;i<quesArray.length;i++){
+        console.log(quesArray[i])
+        if(quesArray[i].value !="")
+        {   
+            errMsg.style.display = "none";
+            if(quesArray[i].value == "right"){
+            score++; 
+            }
+        }else{
+            errMsg.style.display = "block";
+        }
     }
+    finalScore.innerHTML=score;
 })
